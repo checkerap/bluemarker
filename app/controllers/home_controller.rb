44f616e01a2f98dde74d562d2fa4d344c8adf582
Cosmin Stefaniga <cosmin.stefaniga@gmail.com>
2020-06-12 07:15:16 +0000

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   
   def index
-    @events_current = Event.where('start_date <= ?', Date.today).where('end_date >= ?', Date.today)
+    # @events_current = Event.where('start_date <= ?', Date.today).where('end_date >= ?', Date.today)
+    @events_current = Event.all.limit(3)
     @events = Event.all
     
     @news = News.last(3)
